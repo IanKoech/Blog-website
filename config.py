2320c27aba366945a96fd72c9bc784cbf59f1dc3
Ian K <iankoech9@gmail.com>
2020-09-29 12:31:16 +0300

    Added sender-email to email.py file

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa-student:ms@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ian:iankoech@localhost/blog'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     #  email configurations
